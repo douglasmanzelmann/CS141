66eb4d1f197662ea6914453b3d158e018262b249
douglas manzelmann <dougamanzelmann@gmail.com>
2013-12-06 10:21:09 -0500

    String Theory

--- ChapterNine/src/StringTheory.java
+++ ChapterNine/src/StringTheory.java
@@ -19,6 +19,7 @@ public class StringTheory {
             System.out.println("Enter the scores for student #" + row);
             for (int col = 1; col < scores[row].length; col++) {
                 scores[row][col] = input.nextInt();
+
             }
         }
 
@@ -30,6 +31,8 @@ public class StringTheory {
 
         StringBuilder[][] scoreGraphs = new StringBuilder[students][tests+1];
         for (int row = 0; row < scoreGraphs.length; row++) {
+            scoreGraphs[row][0] = new StringBuilder(scores[row][0] / (scores[row].length - 1));
+            scoreGraphs[row][0].append(graphScore(scoreGraphs[row][0].capacity()));
             for (int col = 1; col < scoreGraphs[row].length; col++) {
                 scoreGraphs[row][col] = new StringBuilder(scores[row][col]);
                 scoreGraphs[row][col].append(graphScore(scores[row][col]));
@@ -37,12 +40,25 @@ public class StringTheory {
         }
 
         for (int row = 0; row < scoreGraphs.length; row++) {
-            System.out.println("Scores for student #" + row);
+            System.out.println("Scores for student #" + (row + 1));
             for (int col = 1; col < scoreGraphs[row].length; col++) {
                 System.out.println("Test #" + col + ": " + scoreGraphs[row][col]);
             }
-            System.out.println("Student #" + row + " aveage: " + graphScore(scores[row][0] / (scores[row].length -1) ));
+            System.out.println("Student #" + (row + 1) + " average: " + graphScore(scoreGraphs[row][0].capacity()));
         }
+
+        int maxAverage = Integer.MIN_VALUE;
+        int maxAverageStudent = 0;
+
+        for (int i = 0; i < scoreGraphs.length; i++) {
+            if (scoreGraphs[i][0].length() > maxAverage) {
+                maxAverage = scoreGraphs[i][0].length();
+                maxAverageStudent = i;
+            }
+        }
+
+        System.out.println("The student with the greatest average is student #" + (maxAverageStudent + 1) +
+            " and his/her average is " + maxAverage);
 
     }
 
